d5d2e19bb1d427c5a1ee5d8a8134f38afcd9c56c
AlrightErin27 <erinmontybruce@gmail.com>
2021-07-01 16:57:35 -0700

    fin shortest word

---
+++ shortestWord.js
@@ -0,0 +1,19 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function shortestWord(str) {
+  let arr = str.split(" ");
+  let min = arr[0].length;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length < min) {
+      min = arr[i].length;
+    }
+  }
+  return min;
+}
+
+console.log(shortestWord("a castle in the sky")); //1
+console.log(shortestWord("because everything wont")); //4
+console.log(shortestWord("sky yes please")); //3
