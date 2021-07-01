d988331f71a104fdedbe04e7e4be2e6a90113e59
AlrightErin27 <erinmontybruce@gmail.com>
2021-07-01 14:29:47 -0700

    fin keepHydrated

---
+++ keepHydrated.js
@@ -0,0 +1,24 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+//-------------------------------------------------------------------------------//
+//nathan drinks .5 litres / hour
+// x hrs = ? litres?
+//round return to smallest value
+
+const cycling = (hours) => {
+  let approxLiters = hours * 0.5;
+  const liters = Math.floor(approxLiters);
+  return liters;
+};
+console.log(cycling(6.7));
