821e935242ca73b8b2f24b9f3d096169ba2105f7
AlrightErin27 <erinmontybruce@gmail.com>
2021-07-01 17:01:49 -0700

    short and sweet

--- shortestWord.js
+++ shortestWord.js
@@ -2,17 +2,21 @@
 
 // String will never be empty and you do not need to account for different data types.
 
-function shortestWord(str) {
-  let arr = str.split(" ");
-  let min = arr[0].length;
+// function shortestWord(str) {
+//   let arr = str.split(" ");
+//   let min = arr[0].length;
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].length < min) {
-      min = arr[i].length;
-    }
-  }
-  return min;
-}
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].length < min) {
+//       min = arr[i].length;
+//     }
+//   }
+//   return min;
+// }
+
+const shortestWord = (str) => {
+  return str.split(" ").sort((a, b) => a.length - b.length)[0].length || 0;
+};
 
 console.log(shortestWord("a castle in the sky")); //1
 console.log(shortestWord("because everything wont")); //4
