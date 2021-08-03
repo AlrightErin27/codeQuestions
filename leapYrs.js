da9f61c1d026b8480a8a948d774c47c94c88aeb3
AlrightErin27 <erinmontybruce@gmail.com>
2021-08-02 17:31:13 -0700

    mmkay?

---
+++ leapYrs.js
@@ -0,0 +1,20 @@
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you
+// don't know the rules, here they are:
+
+// years divisible by 4 are leap years
+// but years divisible by 100 are not leap years
+// but years divisible by 400 are leap years
+// Additional Notes:
+
+// Only valid years (positive integers) will be tested, so you don't have to validate them
+
+let isLeapYr = (year) => {
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+};
+
+// console.log(isLeapYr(1234)); //f
+// console.log(isLeapYr(1984)); //t
+// console.log(isLeapYr(2000)); //t
+// console.log(isLeapYr(2010)); //f
+// console.log(isLeapYr(2013)); //f
+console.log(isLeapYr(2013));
