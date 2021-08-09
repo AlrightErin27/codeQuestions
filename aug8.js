f6c4a6e2769d4dc68967647fdae18f60dc2ab91e
AlrightErin27 <erinmontybruce@gmail.com>
2021-08-08 17:26:50 -0700

    hmmm

--- aug8.js
+++ aug8.js
@@ -166,3 +166,62 @@ function toCamelCase(str) {
   return arr[0] + result;
 }
 // console.log(toCamelCase("the_steal-warrior"));
+
+//////////////////////////////////Hashtag Generator//////////////////////////////////
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+// Examples
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+
+function hashtag(str) {
+  let newArr = [],
+    result = "";
+  let arr = str.toLowerCase().split(" ");
+
+  for (let i = 0; i < arr.length; i++)
+    arr[i] !== "" ? newArr.push(arr[i]) : newArr;
+
+  for (let j = 0; j < newArr.length; j++)
+    result += newArr[j][0].toUpperCase() + newArr[j].slice(1);
+
+  result.length + 1 > 140 || result.length === "0" || str === ""
+    ? false
+    : `#${result}`;
+}
+//console.log(hashtag("code" + " ".repeat(140) + "wars"));
+
+//////////////////////////////////Pete The Baker//////////////////////////////////
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths.
+// Can you help him to find out, how many cakes he could bake considering his recipes?
+
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object)
+// and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts
+// (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be
+// considered as 0.
+
+// Examples:
+
+// // must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+// // must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+function cakes(recipe, avail) {
+  let numFlour = avail.flour / recipe.flour;
+  console.log(numFlour);
+  //returns the maxNum of cakes that can be bakes (integer)
+}
+console.log(
+  cakes(
+    { flour: 500, sugar: 200, eggs: 1 },
+    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+  )
+);
